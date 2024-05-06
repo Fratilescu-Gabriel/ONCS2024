71326bea0541fb22fc26b8dcb9c0ed0651479e89
Fratilescu-Gabriel <138109331+Fratilescu-Gabriel@users.noreply.github.com>
2024-05-06 13:17:15 +0300

    v1.0 github test with new env

--- testing_space/test.py
+++ testing_space/test.py
@@ -10,4 +10,4 @@ a = np.array([[-2.58289208,  0.43014843, -1.24082018, 1.59572603],
               [ 0.20484034,  0.34784527,  1.96979195, 0.51992837]])
 
 df = pd.DataFrame(a)
-df.to_csv(r"C:\Users\Fratilescu Gabriel\Desktop\\test.csv" , mode='a')
+df.to_csv("test.csv" , mode='x')
